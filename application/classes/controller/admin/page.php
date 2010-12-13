ff3f7ad85580317279c4ee04f884a37fac8de0c6
Jon Ferrer <jon@jonferrer.com>
2010-12-12 22:24:07 -0500

    use AuthPage controller

--- application/classes/controller/admin/page.php
+++ application/classes/controller/admin/page.php
@@ -1,8 +1,9 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 
-class Controller_Admin_Page extends Controller_Page 
+class Controller_Admin_Page extends Controller_AuthPage 
 {
 	public $template_name = 'admin';
+    public $auth_required = array('login', 'admin'); // user must be logged in and admin!
 } 
 
 
