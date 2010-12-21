cf83deb3e2de7cde39e5f3900bcfb5f543c0bd50
Jon Ferrer <jon@jonferrer.com>
2010-12-20 21:30:40 -0500

    surface venue id for event from eventservice response

--- application/classes/serviceclient/driver/event.php
+++ application/classes/serviceclient/driver/event.php
@@ -4,6 +4,7 @@ class ServiceClient_Driver_Event
 {
     public $id = NULL;
     public $name = NULL;
+    public $datetime = NULL;
     public $date = NULL;
     public $time = NULL;
     public $description = NULL;
@@ -12,8 +13,11 @@ class ServiceClient_Driver_Event
     {
         $this->id          = $event->attributes('id'); 
         $this->name        = $event->name->value();
-        $this->date        = Date::formatted_time($event->datetime->value(), 'm/d/y', 'America/New_York'); // @TODO: account for timezone of group! ie: NY.com vs LA.com
-        $this->time        = Date::formatted_time($event->datetime->value(), 'g:i A', 'America/New_York'); 
+        $this->datetime    = $event->datetime->value();
+        $this->date        = Date::formatted_time($this->datetime, 'm/d/y', 'America/New_York'); // @TODO: account for timezone of group! ie: NY.com vs LA.com
+        $this->time        = Date::formatted_time($this->datetime, 'g:i A', 'America/New_York'); 
         $this->description = $event->description->value();
+
+        $this->venue_id = $event->venue->attributes('id');
     }
 }
