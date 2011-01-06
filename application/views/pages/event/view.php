7f0be15e308dd8bf4221479ab601adc803b5c7ae
Jon Ferrer <jon@jonferrer.com>
2011-01-06 14:33:06 -0500

    rough event view

--- application/views/pages/event/view.php
+++ application/views/pages/event/view.php
@@ -1,12 +1,22 @@
-<?php echo Request::instance()->param('group'); ?>
+<div id="event_header">
+    <h2><?php echo $event->name; ?></h2>
+    <p><?php echo $event->venue->name; ?></p>
+    <p><?php echo Date::formatted_time($this->datetime, 'l, F j, Y'); ?></p>
+    <?php /* <p><?php echo $event->subtitle; ?></p> */ ?>
+</div>
 
-<h2><?php echo $event->name; ?></h2>
+<?php /*
 <div class="dateline">
     <span class="date">Date: <?php echo $event->date; ?></span><br/>
     <span class="time">Time: <?php echo $event->time; ?></span>
 </div>
+*/ ?>
 
-<h3>Description</h3>
-<div class="event-description">
+<h3>Highlights</h3>
+<div class="event-highlights">
+    <p><strong><?php echo $event->name; ?></strong></p>
+    <p><?php echo $event->venue->name; ?></p>
+    <p><?php echo Date::formatted_time($this->datetime, 'l, F j, Y'); ?></p>
+
     <?php echo $event->description; ?>
 </div>
